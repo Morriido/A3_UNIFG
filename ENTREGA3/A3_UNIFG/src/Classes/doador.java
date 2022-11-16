d38b654d8e2f94086465b529dea49b81825fe4af
Kaio Oliveira <80167194+Morriido@users.noreply.github.com>
2022-11-16 15:45:30 -0300

    Add files via upload

---
+++ ENTREGA3/A3_UNIFG/src/Classes/doador.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import java.util.Scanner;
+
+public class doador {
+    Scanner input = new Scanner(System.in);
+    public String nome;
+    public String tipo_sanguineo;
+    public int idade;
+    public int ultima_doacao;
+    public int CPF;
+    public String banco;
+
+    public void doar(){
+        System.out.println("digite a data de hoje sem / (exemplo: 21/08/22 = 210822 ");
+        ultima_doacao = input.nextInt();
+        System.out.println(nome + " doou sangue do tipo " + tipo_sanguineo);
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        if(ultima_doacao == 0){
+            return "nome: " + nome +
+        "\ntipo sanguineo:  " + tipo_sanguineo + 
+        "\nidade: " + idade + 
+        "\nultima doação: nunca" +
+        "\nCPF: " + CPF + 
+        "\nbanco de sangue: " + banco;
+        }else{
+            return "nome: " + nome +
+        "\ntipo sanguineo:  " + tipo_sanguineo + 
+        "\nidade: " + idade + 
+        "\nultima doação: " + ultima_doacao +
+        "\nCPF: " + CPF + 
+        "\nbanco de sangue: " + banco;
+        }
+        
+    }
+}
---
+++ ENTREGA3/A3_UNIFG/src/Classes/usuario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author conta
+ */
+public class usuario {
+    //atributos
+    public String login;
+    public String senha;
+    public int ID;
+    
+    //construtores
+    public void Usuario(String login, String senha, int ID){
+        this.login = login;
+        this.senha = senha;
+        this.ID = ID;
+    }
+    //funcionalidades
+    public void logar(){
+        
+    }
+    public void registrar(){
+        
+    }
+}
