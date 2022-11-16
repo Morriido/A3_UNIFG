0e56d9119da8b40cf9dc8bb836a73e8dc4e9a691
Kaio Oliveira <80167194+Morriido@users.noreply.github.com>
2022-11-16 18:23:02 -0300

    Add files via upload
    Previa da entrega 3

---
+++ ENTREGA3/A3_UNIFG/src/Classes/sangue.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import Telas.Menu;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author conta
+ */
+public class sangue {
+    public int quant;
+    public String tipo;
+    public String banco;
+    public String dono;
+}
+
+
